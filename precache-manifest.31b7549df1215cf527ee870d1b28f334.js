73e502276d4a4d1bb46b54caa99e6670b846eecc
hecktors <tohe.webdev@gmail.com>
2020-09-14 13:32:41 +0200

    Updates

---
+++ precache-manifest.31b7549df1215cf527ee870d1b28f334.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a9e19e07c69130a7c7b5b8d7fd8bf1c",
+    "url": "/react-pomodoro-clock/index.html"
+  },
+  {
+    "revision": "5f2bbc2ac43c4b873bc1",
+    "url": "/react-pomodoro-clock/static/css/main.ff470c5c.chunk.css"
+  },
+  {
+    "revision": "5a57c8ae8c70d57e655c",
+    "url": "/react-pomodoro-clock/static/js/2.84192d4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pomodoro-clock/static/js/2.84192d4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f2bbc2ac43c4b873bc1",
+    "url": "/react-pomodoro-clock/static/js/main.9c827e9e.chunk.js"
+  },
+  {
+    "revision": "f5ab37633c093f56a4ee",
+    "url": "/react-pomodoro-clock/static/js/runtime-main.23f6369a.js"
+  }
+]);
